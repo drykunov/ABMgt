aa017810dc32f580f807adcf07acdcc07a67671c
Dmitrii Rykunov <dprykunov@gmail.com>
2016-04-16 03:02:37 +0300

    Fixed bugs and improved readability

--- test on mco.R
+++ test on mco.R
@@ -1,11 +1,13 @@
 require(mco)
 
 
-pris_dilemma <- list(matrix(c(-1, 0, -3, -2), 2, 2), matrix(c(-1, -3, 0, -2), 2, 2))
+game_matrix <- list(matrix(c(-1, 0, -3, -2), 2, 2), 
+                    matrix(c(-1, -3, 0, -2), 2, 2))
 
 
 normalizeStrategies <- function(s) {
-    if(!is.numeric(s) || length(s) != 4) stop("Strategies vector form is inapropriable!")
+    if(!is.numeric(s) || length(s) != 4) stop("Strategies vector form 
+                                              is inapropriable!")
     
     sumP1 <- sum(s[1:2])
     sumP2 <- sum(s[3:4])
@@ -19,8 +21,8 @@ normalizeStrategies <- function(s) {
     return(s)
 }
 
-chooseS <- function(s) {
-    #if(sum(s) != 2) stop("Startegies are not normilized!")
+choosePureStrategies <- function(s) {
+    #if(sum(s) != 2) stop("Strategies are not normilized!")
     
     P1move <- sample(1:2, 1, prob = s[1:2])
     P2move <- sample(3:4, 1, prob = s[3:4])
@@ -29,7 +31,8 @@ chooseS <- function(s) {
 }
 
 calcScore <- function(ps) {
-    if(!is.numeric(ps) || length(ps) != 2) stop("Pure strategies set is inapropriate!")
+    if(!is.numeric(ps) || length(ps) != 2) stop("Pure strategies set 
+                                                is inapropriate!")
     
     P1score <- game_matrix[[1]][ps[1], ps[2] - 2]
     P2score <- game_matrix[[2]][ps[1], ps[2] - 2]
@@ -39,7 +42,8 @@ calcScore <- function(ps) {
 
 
 totalScore <- function(s) {
-    if(!is.numeric(s) || length(s) != 4) stop("Input from alg is inapropriate!")
+    if(!is.numeric(s) || length(s) != 4) stop("Input from alg 
+                                              is inapropriate!")
     
     # Initialize score table
     scores <- matrix(numeric(200), nrow = 100, ncol = 2)
@@ -49,7 +53,7 @@ totalScore <- function(s) {
     
     for(i in seq(nrow(scores))) {
         # Derive pure strategies
-        pure_strategies <- chooseS(norm_strategies)
+        pure_strategies <- choosePureStrategies(norm_strategies)
         
         # Caclulate score
         scores[i, ] <- calcScore(pure_strategies)
@@ -58,21 +62,36 @@ totalScore <- function(s) {
     return(scores)
 }
 
+
+# Convert maximizing GT problem to minimizing problem
 gtMinProb <- function(x) {
     return(colSums(totalScore(x))*(-1))
 }
 
 
 
-r1 <- nsga2(gtMinProb, 4, 2,
+raw_results <- nsga2(gtMinProb, 4, 2,
       generations = 100,
       popsize = 100,
       lower.bounds = rep(0, 4),
       upper.bounds = rep(1, 4))
 
+# Convert values back to their orginal signs
+raw_results$value <- raw_results$value * (-1)
 
-norm_r1 <- aperm(apply(r1$par, 1, normalizeStartegies))
-tr1 <- cbind(round(norm_r1, 5), r1$value)
-results_first_run <- tr1[order(tr1[, 5] + tr1[, 6]), ]
+
+normalized_raw_params <- aperm(apply(raw_results$par, 1, normalizeStrategies))
+params_with_values <- cbind(round(normalized_raw_params, 5), 
+                            raw_results$value)
+results_first_run <- params_with_values[order(params_with_values[, 5] + 
+                                                  params_with_values[, 6], 
+                                              decreasing = TRUE), ]
+results_first_run <- as.data.frame(results_first_run)
+colnames(results_first_run) <- c("P1 cooperates", 
+                                 "P1 defects", 
+                                 "P2 cooperates", 
+                                 "P2 defects", 
+                                 "P1 SCORE", 
+                                 "P2 SCORE")
 print(results_first_run)
-plot(r1)
+plot(raw_results)

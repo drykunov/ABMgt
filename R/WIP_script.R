03167d235801394d6b6d57e277028e8b55756a24
Dmitrii Rykunov <dprykunov@gmail.com>
2016-06-23 15:16:38 +0300

    New default variables and tests introduced

--- R/WIP_script.R
+++ R/WIP_script.R
@@ -1,10 +1,30 @@
+library(testthat)
+
 source("R/gt_model.R")
+source("R/evolutionary_functions.R")
+
+
+# Variables ----------------------------------------------------
+
+# Defaults
+strat.coding.min <- 0
+strat.coding.max <- 1
+
+gm <- GM$prisoners_dilemma
+
+popsize <- 50
+dropout_rate <- 0.5
+mutation_rate <- 0.2
+mutation_expansion_rate <- 1.3
+crossover_rate <- 0.3
+
+# Tests -------------------------------------------------------------------
 
 set.seed(111)
 
 gm <- GM$prisoners_dilemma
-pop <- InitializePopulation(100, gm)
-pop_norm <- NormalizePopulation(pop)
+pop_in <- InitializePopulation(50, gm)
+pop <- NormalizePopulation(pop_in)
 
 # print(pop_norm)
 # 
@@ -12,7 +32,18 @@ pop_norm <- NormalizePopulation(pop)
 
 
 
+ss <- TournamentSelecton(pop, "p2", index = 6, npairs = 40)
+dd <- TournamentSelecton(pop, "p1", index = 7, npairs = 40)
+
+
 # # Sort strategies to their distance from restrictions
 # pop[[1]] %>% mutate(deviation = abs(Cooperate + Defect - 1)) %>% arrange(deviation)
 
 test_dir("tests/")
+
+
+# 
+# pop[[2]][1:8, ] <- matrix(c(rep(c(1,0), 4), rep(c(0,1), 4)), 8, 2, byrow = TRUE)
+# 
+# fits <- GetFitValues(population = pop, ngames = 50)
+# sortedPop <- SortToFit(population = pop, fitValues = fits)
